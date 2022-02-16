7da49ed3747dd8add26e6093b5384d34b7d87754
Scott <scott.anderson10012@gmail.com>
2022-02-16 17:49:40 +0000

    created 3D sound test

--- sound/generator.py
+++ sound/generator.py
@@ -4,16 +4,30 @@ from openal import *
 # import the time module, for sleeping during playback
 import time
 
-# open our wave file
+# open our mono wave file
 source = oalOpen("test.wav")
+print("test that the wav is mono:")
+# source.AL_FORMAT_MONO8
+print("Listener start location: ")
+print(oalGetListener().position)
+for i in range(0,5):
+    # and  start playback
+    source.play()
+    i+=1
+    # check if the file is still playing
+    while source.get_state() == AL_PLAYING:
+	    # wait until the file is done playing
+	    time.sleep(1)
+    oalGetListener().move((1,0,0))
+    print("Listener following location: ")
+    print(oalGetListener().position)
+    
+    if i==3: 
+        oalGetListener().set_orientation((-1, 0, 0, 0, 1, 0))
+    
 
-# and start playback
-source.play()
 
-# check if the file is still playing
-while source.get_state() == AL_PLAYING:
-	# wait until the file is done playing
-	time.sleep(1)
+
 
 # release resources (don't forget this)
 oalQuit()
